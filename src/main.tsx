a2ccf4f6ea68c9368dfc780caa30f6b7c6179cb9
RealGecko <alexandrbezenkov@gmail.com>
2024-09-06 13:25:00 +0500

    Added `PrimeReactProvider` with `unstyled: true`

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
+import { PrimeReactProvider } from "primereact/api"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider value={{ unstyled: true }}>
+      <App />
+    </PrimeReactProvider>
   </React.StrictMode>
 )
